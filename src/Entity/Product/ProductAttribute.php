a91d8365bedc4c48ed0b8ec9d91b0df6b3e035f9
DAIF Abderrahman <daif.abderrahman@gmail.com>
2023-02-25 23:49:31 +0100

    [Maintenance] Use PHP 8 attributes instead of annotations.

--- src/Entity/Product/ProductAttribute.php
+++ src/Entity/Product/ProductAttribute.php
@@ -8,12 +8,8 @@ use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Attribute\Model\AttributeTranslationInterface;
 use Sylius\Component\Product\Model\ProductAttribute as BaseProductAttribute;
 
-/**
- * @ORM\Entity
- * @ORM\Table(name="sylius_product_attribute")
- */
 #[ORM\Entity]
-#[ORM\Table(name: 'sylius_product_attribute')]
+#[ORM\Table(name: "sylius_product_attribute")]
 class ProductAttribute extends BaseProductAttribute
 {
     protected function createTranslation(): AttributeTranslationInterface
--- src/Entity/Taxonomy/Taxon.php
+++ src/Entity/Taxonomy/Taxon.php
@@ -8,12 +8,8 @@ use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Core\Model\Taxon as BaseTaxon;
 use Sylius\Component\Taxonomy\Model\TaxonTranslationInterface;
 
-/**
- * @ORM\Entity
- * @ORM\Table(name="sylius_taxon")
- */
 #[ORM\Entity]
-#[ORM\Table(name: 'sylius_taxon')]
+#[ORM\Table(name: "sylius_taxon")]
 class Taxon extends BaseTaxon
 {
     protected function createTranslation(): TaxonTranslationInterface
